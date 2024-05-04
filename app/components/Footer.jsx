46c6788684cfb05acf91cc3b8b5b3f4e04ad9196
Andy <andrewohejiedeogbu@yahoo.com>
2024-05-04 18:48:36 +0100

    move

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,7 +8,42 @@ function Footer() {
       <h1>Book one of our professional Photographer that will add color to your occasion</h1>
       </div>
       <div className={styles.line}>
-        <h1>Hellp</h1>
+        <h1>We are Realiabe and Proffissional </h1>
+      </div>
+      <div className={styles.footerimage}>
+        <div className={styles.footimg}>
+        <img 
+         src="/images/x.jpg"
+         alt="x"
+         width={30}
+         height={30}
+         className='img'
+         />
+         <img 
+         src="/images/x.jpg"
+         alt="x"
+         width={30}
+         height={30}
+         className='img'
+         />
+         <img 
+         src="/images/x.jpg"
+         alt="x"
+         width={30}
+         height={30}
+         className='img'
+         />
+         </div>
+         <div className={styles.footernave}>
+         <nav className={styles.footnav1}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Gallery</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+                     </div>
       </div>
     </div>
   )
