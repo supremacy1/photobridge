4b3df5e941352b117a7746c9822c18350497cdbd
Andy <andrewohejiedeogbu@yahoo.com>
2025-02-05 14:32:30 +0100

    load

--- app/picture/page.jsx
+++ app/picture/page.jsx
@@ -1,19 +1,29 @@
-"use client"
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+// "use client"
+// import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
 
-export class page extends Component {
-  static propTypes = {
+// export class page extends Component {
+//   static propTypes = {
 
-  }
+//   }
 
-  render() {
-    return (
-      <div>
-        <h1>Image</h1>
-      </div>
-    )
-  }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Image</h1>
+//       </div>
+//     )
+//   }
+// }
+
+// export default page
+"use client";  
+import React from "react";  
+
+export default function Page() {  
+  return (  
+    <div>  
+      <h1>Image</h1>  
+    </div>  
+  );  
 }
-
-export default page
